4cf8855e664ffa92abc84fe4984ea0d1a386e875
xubaodian <472784995@qq.com>
2018-12-26 19:29:53 +0800

    修改

--- demo/js/index.js
+++ demo/js/index.js
@@ -1,68 +1,93 @@
-import bgImg from '../../asserts/room.jpg';
-import serverImg from '../../asserts/server.jpg';
-import { setBkImg, setImgInvert, setImgAlpha} from '../../src/Image/graphicTools'
-
+import roomImg from '../../asserts/room.jpg'; 
+//获取绘图上下文
+let ctx = document.getElementById('canvas').getContext('2d');
+let bkimg = new Image();
+bkimg.src = roomImg;
+let timer = null;
+//旋转的角度
 let startAngle = 0;
-
-let bkImg = new Image();
-bkImg.src = bgImg;
-
-let servImg = new Image();
-servImg.src = serverImg;
-
-//
-let dom = document.getElementById('app');
-let ctx = addCanvas(dom);
-
-//按钮事件
-document.getElementById('addBk').onclick = () => {
-  setBkImg(ctx, bkImg);
-  drawEllipse(ctx, 4);
-  setInterval(() => {
-    ctx.clearRect(0, 0, ctx.width, ctx.height);
-    startAngle += 0.01*Math.PI;
-    if (startAngle > 2*Math.PI) {
-      startAngle -= 2*Math.PI;
+//确保图片加载完成再绘制到
+if (!bkimg.complete) {
+ //用定时器检查图片是否绘制完成
+  timer = setInterval(() => {
+    //两张图片加载完成
+    if (bkimg.complete) {
+       //清除定时器
+      clearInterval(timer);
+      timer = null;
+      //绘制
+      drawAll();
     }
-    setBkImg(ctx, bkImg);
-    drawEllipse(ctx, 5);
-  }, 100);
-}
-
-function addCanvas(dom) {
-  let canvas = document.createElement('canvas');
-  canvas.width = dom.offsetWidth;
-  canvas.height = dom.offsetHeight;
-  canvas.style.position = 'absolute';
-  canvas.style.top = 0;
-  canvas.style.left = 0;
-  dom.appendChild(canvas);
-  let ctx = canvas.getContext('2d');
-  ctx.width = dom.offsetWidth;
-  ctx.height = dom.offsetHeight;
-  return ctx;
+  }, 200);
+} else {
+  drawAll();
 }
 
 
-function drawEllipse(ctx, len) {
-  ctx.save();
-  ctx.translate(240, 340);
-  ctx.stokeStyle = '#000';
-  ctx.globalAlpha = 1;
-  ctx.scale(1, 0.2);
-  ctx.arc(0, 0, 200, 0, 2*Math.PI);
-  ctx.stroke();
-  let averAngle = 2*Math.PI/len;
-  //添加旋转图标
-  for (let i = 0; i < len; i++) {
-    let angle = startAngle + i * averAngle;
-    if (angle > 2*Math.PI) {
-      angle -= 2*Math.PI;
+    function drawAll() {
+      //清除原来的图层
+      ctx.clearRect(0, 0, 500, 400);
+      addBk();
+      drawEllipse();
+      drawRotateIcon(5);
+      startAngle += 0.004*Math.PI;
+        if (startAngle > 2*Math.PI) {
+          startAngle -= 2*Math.PI;
+        }
+      //不断重新绘制旋转图标的位置,使用此函数代替定时器
+      requestAnimationFrame(() => {
+        drawAll();
+      });
     }
-    let x = 0, y =0;
-    x = 200*Math.cos(angle);
-    y = 200*Math.sin(angle);
-    ctx.drawImage(servImg, x - 20, y - 5*30, servImg.width, 5*servImg.height); 
-  }
-  ctx.restore();
-}
+   
+    //添加楼房背景
+    function addBk() {
+      ctx.save();
+      ctx.globalAlpha = 0.8;
+      ctx.drawImage(bkimg, 0, 0, 500, 400);
+      ctx.restore();
+    }
+
+    //画椭圆
+    function drawEllipse() {
+      ctx.save();
+      ctx.translate(250, 260);
+      ctx.beginPath();
+      ctx.stokeStyle = '#A0A0A0';
+      ctx.globalAlpha = 0.5;
+      ctx.scale(1, 0.2);
+      ctx.arc(0, 0, 200, 0, 2*Math.PI);
+      ctx.closePath();
+      ctx.stroke();
+      ctx.restore();
+    }
+
+    //画旋转的圆
+    function drawRotateIcon(len) {
+      ctx.save();
+      ctx.translate(250, 260);
+      ctx.fillStyle="red";
+      let averAngle = 2*Math.PI/len;
+      for (let i = 0; i < len; i++) {
+        let angle = startAngle + i * averAngle;
+        if (angle > 2*Math.PI) {
+          angle -= 2*Math.PI;
+        }
+        if (angle < 1.65*Math.PI && angle > 1.3*Math.PI) {
+          ctx.globalAlpha = 0.1;
+        } else {
+          ctx.globalAlpha = 0.8;
+        }
+        let x = 0, y =0;
+        x = 200*Math.cos(angle);
+        y = 0.2*200*Math.sin(angle);
+        ctx.beginPath();
+        ctx.arc(x, y, 10, 0, 2*Math.PI);
+        ctx.closePath();
+        ctx.stroke();
+        ctx.fill();
+        //若要绘制图片围着楼宇转，则在此处添加图片，如下
+        //ctx.drawImage(rotateImg, x - 0.5*serverImg.width , y - 0.5*serverImg.height, serverImg.width, serverImg.height); 
+      }
+      ctx.restore();
+    }
